1e6eabdb8f2183e84eb558b8154e98cc829f2568
cosorio <carlosivanosorio@gmail.com>
2024-07-05 13:52:19 -0500

    Creating new type to access service

--- src/modules/access-keys/types/access-key-response.type.ts
+++ src/modules/access-keys/types/access-key-response.type.ts
@@ -1,8 +1,29 @@
 export type AccessKeyResponseType = {
-  binNumber: number;
-  idSmartBox: number;
-  idLocker: number;
-  lastStatusUpdate: string;
-  Status: string;
-  key?: string;
+  id: number;
+  suppressGuestNotifications: boolean;
+  anonymous: boolean;
+  returnReminder: boolean;
+  requirePhotoVerification: boolean;
+  suppressInstructions: boolean;
+  requireTravelerID: boolean;
+  bookingCode: number;
+  user: {
+    email: string;
+  };
+  accessCode: number;
+  allowSetup: boolean;
+  key: {
+    id: number;
+    currentLocation: {
+      id: number;
+      name: string;
+    };
+    name: string;
+    homeLocation: {
+      id: number;
+      name: string;
+    };
+    serialNumber: string;
+  };
+  confirmed: boolean;
 };
