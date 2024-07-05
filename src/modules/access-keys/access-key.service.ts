5e09b9e466a1ef1f1d36b6495a33051ed8c7239f
cosorio <carlosivanosorio@gmail.com>
2024-07-05 13:52:42 -0500

    Fixing access service modules

--- src/modules/access-keys/access-key.service.ts
+++ src/modules/access-keys/access-key.service.ts
@@ -1,51 +1,48 @@
 import axios from 'axios';
-import { AuthDto } from '@common/dtos';
+import { AccessResponseDto } from './dto/response-access-key.dto';
 import { AccessKeyResponseType } from './types/access-key-response.type';
+import { SendAccessKeyDtoType } from './dto/send-access-key-service.dto';
 
 export class AccessKeyService {
   async sendAccessKey({
     user,
     sendAccessKeyData,
-  }: SendAccessKeyDtoType): Promise<any> {
+  }: SendAccessKeyDtoType): Promise<AccessResponseDto> {
     try {
       const authHeader = `Basic ${Buffer.from(`${user.username}:${user.password}`).toString('base64')}`;
       const { keyId, email, mobile, ...accessKeyData } = sendAccessKeyData;
-      const userInfo = email ? { email: email } : { mobile: mobile };
-
-      await axios.post<AccessKeyResponseType>( //TODO: Add type for response and do something with response
+      const userInfo = email
+        ? { user: { email: email } }
+        : { user: { mobile: mobile } };
+      const databody = {
+        ...userInfo,
+        key: { id: keyId },
+        ...accessKeyData,
+      };
+      const { data } = await axios.post<AccessKeyResponseType>(
         `${process.env.HOST_KEYCAFE}/access`,
+        databody,
         {
           headers: {
             Authorization: authHeader,
-          },
-          data: {
-            ...userInfo,
-            key: { serialNumber: keyId },
-            ...accessKeyData,
           },
         },
       );
 
       return {
         message: 'Access key sent successfully',
+        idAccess: data.id,
+        bookingcode: data.bookingCode,
+        email: data.user.email,
+        keyId: data.key.id,
+        keyName: data.key.name,
       };
     } catch (error) {
-      throw new Error('Error sending access key');
+      if (axios.isAxiosError(error)) {
+        throw new Error(`Axios error: ${error.message}`);
+      } else {
+        throw new Error('Unexpected error sending access key');
+      }
     }
   }
 }
-
-type SendAccessKeyDtoType = {
-  user: AuthDto;
-  sendAccessKeyData: {
-    keyId: number;
-    email?: string;
-    mobile?: string;
-    accessStartDate?: string;
-    accessStartTime?: string;
-    accessEndDate?: string;
-    accessEndTime?: string;
-    name?: string;
-    returnReminder?: boolean;
-  };
-};
